8934f2d1d630899d68560071f3efa6d5d9eae968
hossam hassan <hossamhassan889@gmail.com>
2022-10-06 19:59:20 +0200

    online update

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Registerd;
 class ShopController extends Controller
 {
-    //
+  public function sendData(Request $request){
+        $registerd = Registerd::create($request->all());
+        return redirect('https://www.facebook.com/');
+    }
 }
